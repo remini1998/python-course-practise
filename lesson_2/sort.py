5f692c5a561771190733396801421a1ecc92dcbf
彼端。 <i@iaside.com>
2018-11-01 16:10:41 +0800

    fix bug

--- lesson_2/sort.py
+++ lesson_2/sort.py
@@ -6,7 +6,7 @@ from lesson_2.decorator import timer
 
 
 @timer
-def bubble_sort(input_list: List[int]):
+def bubble_sort(input_list: List[int]) -> List[int]:
     input_list = input_list.copy()
     for x in range(input_list.__len__(), 0, -1):
         for y in range(x - 1):
@@ -16,16 +16,15 @@ def bubble_sort(input_list: List[int]):
 
 
 @timer
-def selection_sort(input_list: List[int]):
+def selection_sort(input_list: List[int]) -> List[int]:
     input_list = input_list.copy()
     for i in range(input_list.__len__()):
-        min_index = input_list.index(min(input_list[i:]))
+        min_index = input_list.index(min(input_list[i:]), i)
         input_list[i], input_list[min_index] = input_list[min_index], input_list[i]
     return input_list
 
 
 if __name__ == '__main__':
-
     array = list(np.random.randint(100, size=30000))
 
     print("raw array:", array)
